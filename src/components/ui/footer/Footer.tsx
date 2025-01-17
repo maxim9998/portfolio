ea455ab56646ac404a40b288c149742a56a81094
maxim9998 <maxim9998@gmail.com>
2025-01-18 02:01:07 +0400

    footer padding fix

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -3,7 +3,7 @@ import RightPart from "./rightPart/RightPart";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t flex flex-col xl:flex-row items-center xl:items-start justify-between gap-10 xl:gap-0 p-10 xs:p-4">
+    <footer className="w-full border-t flex flex-col xl:flex-row items-center xl:items-start justify-between gap-10 xl:gap-0 p-10 xs:px-4 xs:pt-14">
       <LeftPart />
       <RightPart />
     </footer>
